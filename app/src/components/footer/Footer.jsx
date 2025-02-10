7451e9557cf6a2971fa109d416f7ad7b958b8fb8
Andrew Clark <aiandrew631@gmail.com>
2025-02-09 19:50:05 -0600

    Updated Footer component layout, repositioned icons to the left and company info to the right.

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,17 +1,18 @@
 // FILE: app/src/components/footer/Footer.jsx
+
 import React from 'react'
 import { LuTwitter, LuGithub, LuCopyright } from 'react-icons/lu'
 
 export default function Footer() {
     return (
         <footer className='mt-16 text-white'>
-            {/* 1. Thin pulsing accent bar at the top */}
+            {/* 1. Thin pulsing accent bar across the top */}
             <div className='h-1 w-full bg-pulse-gradient animate-colorPulse' />
 
             {/* 2. Main footer area */}
-            <div className='bg-brandGray-900 py-4 px-4 text-center'>
-                <div className='flex flex-col items-center space-y-3'>
-                    {/* Icons Row (centered) */}
+            <div className='bg-brandGray-900 py-4 px-4'>
+                <div className='flex justify-between items-center'>
+                    {/* Icons on the left */}
                     <div className='flex space-x-6'>
                         <a
                             href='https://twitter.com/your_profile'
@@ -31,7 +32,7 @@ export default function Footer() {
                         </a>
                     </div>
 
-                    {/* Company Info Row (centered) */}
+                    {/* Company info on the right */}
                     <div className='flex items-center space-x-2'>
                         <span>Clark Company Limited</span>
                         <LuCopyright className='h-5 w-5' />
